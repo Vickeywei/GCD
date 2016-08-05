4be86e665981d6e8f4ff6b7b9b32aa57db28626e
wei qi <weiqi@hzdracom.com>
2016-08-05 23:16:49 +0800

    分享

---
+++ Pthread分享/Pthread分享/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pthread分享
+//
+//  Created by 魏琦 on 16/6/24.
+//  Copyright © 2016年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
